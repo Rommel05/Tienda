affc7473c2c50ef28e11c7a04a1a5bde44e85a82
Rommel <rrommel5@gmail.com>
2024-10-24 11:17:13 +0200

    V1

---
+++ src/Controller/PageController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        return $this->render('page/index.html.twig');    }
+
+    #[Route('/about', name: 'about')]
+    public function about(ManagerRegistry $doctrine): Response
+    {
+        return $this->render('page/about.html.twig');
+    }
+
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+        return $this->render('page/service.html.twig', []);
+    }
+
+    #[Route('/price', name: 'price')]
+    public function price(): Response
+    {
+        return $this->render('page/price.html.twig', []);
+    }
+
+    #[Route('/team', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('page/team.html.twig', []);
+    }
+
+    #[Route('/testimonial', name: 'testimonial')]
+    public function testimonial(): Response
+    {
+        return $this->render('page/testimonial.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    public function teamTemplate(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('partials/_team.html.twig',compact('team'));
+    }
+
+
+}
