cc8f53cb3c5830488401d398aed65216cf990a01
Rommel <rrommel5@gmail.com>
2024-10-29 13:03:39 +0100

    tienda

--- public/js/app.js
+++ public/js/app.js
@@ -16,5 +16,31 @@
         infoProduct.modal('hide');
     });
 
-    const cart = $();
+    const cart = $("#cart-modal");
+    $( "a.open-cart-product" ).click(function(event) {
+        event.preventDefault();
+        const id = $( this ).attr('data-id');
+        const href = `/cart/add/${id}`;
+        $.get( href, function(data) {
+            $( cartModal ).find( ".name" ).text(data.name);
+            $( cartModal ).find( "#quantity" ).val(data.quantity);
+            $( cartModal ).find( ".img-thumbnail" ).attr("src", "/img/" + data.photo);
+            totalItems.text(data.totalItems);
+            cartModal.modal('show');
+            const updateButton = cartModal.find("#data-container .update")
+            updateButton.unbind();
+            updateButton.click(function(event){
+                event.preventDefault();
+                var hrefUpdate = "/cart/update/" + id;
+                //Hacer un post a update con la cantidad introducida por el usuario
+                hrefUpdate += "/" + $( cartModal ).find( "#quantity" ).val();
+                $.post( hrefUpdate, {}, function(data) {
+                    totalItems.text(data.totalItems);
+                });
+            });
+        })
+    });
+    $(".closeCart").click(function (e) {
+        cartModal.modal('hide');
+    });
 })();
